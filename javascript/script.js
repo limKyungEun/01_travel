5a2712a2d3a368bfd9ad9e287210fa7b72febc55
LimKyungEun <gim970143@gmail.com>
2024-03-08 11:55:57 +0900

    html,css 수정

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,12 @@
 //JavavScript Document
 
-$(document).ready(function(){
+
     $(".list>li").mouseover(function(){
         var submenu = $(this).children(".submenu").slideDown;});
     
     $(".list>li").mouseleave(function(){
         $(this).children(".submenu").stop().slideUp();});
-});
+
     
                 
  start();
